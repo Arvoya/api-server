762d16cec5f4c41cb94ec6f4321244c568de2d62
Isai Chaidez <isaichaidez@gmail.com>
2024-04-06 19:26:17 -0700

    server is working, tests are good, error handlers handle erros

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# api-server
+# API Server
+
+[Deployment](https://api-server-71tr.onrender.com)
+
+## Getting Started
+
+### Requirements
+
+For development, you will only need [Node](http://nodejs.org/) installed in your
+environment.
+Please use the appropriate [Editorconfig](http://editorconfig.org/) plugin for your
+Editor (not mandatory).
+
+### Install
+
+    git clone git@github.com:Arvoya/api-server.git
+    cd PROJECT
+    npm install
+
+### Configure app
+
+Any environment configuration steps.
+
+### Start & watch
+
+    npm run dev
+    npm start
+
+## Architecture
+
+Describe how the project is structured.
+
+### Languages & tools
+
+* JavaScript
+* Node
+* Express
+* Cors
+* Dotenv
+* Jest
+* Nodemon
+* Supertest
+* Sequelize
+* sqlite3
+
+## Change Log
+
+0.0.1 - 2024-04-06
--- src/routes/yogi.js
+++ src/routes/yogi.js
@@ -5,29 +5,49 @@ const router = express.Router();
 
 const YogiCollection = new Collection(Yogi);
 
-router.get("/", async (req, res) => {
-  let yogi = await YogiCollection.read();
-  res.send(yogi);
+router.get("/", async (req, res, next) => {
+  try {
+    let yogi = await YogiCollection.read();
+    res.send(yogi);
+  } catch (e) {
+    next(e);
+  }
 });
 
-router.get("/:id", async (req, res) => {
-  let yogi = await YogiCollection.read(req.params.id);
-  res.send(yogi);
+router.get("/:id", async (req, res, next) => {
+  try {
+    let yogi = await YogiCollection.read(req.params.id);
+    res.send(yogi);
+  } catch (e) {
+    next(e);
+  }
 });
 
-router.post("/", async (req, res) => {
-  let yogi = await YogiCollection.create(req.body);
-  res.json(yogi);
+router.post("/", async (req, res, next) => {
+  try {
+    let yogi = await YogiCollection.create(req.body);
+    res.json(yogi);
+  } catch (e) {
+    next(e);
+  }
 });
 
-router.put("/:id", async (req, res) => {
-  let yogi = await YogiCollection.update(req.params.id, req.body);
-  res.send(yogi);
+router.put("/:id", async (req, res, next) => {
+  try {
+    let yogi = await YogiCollection.update(req.params.id, req.body);
+    res.send(yogi);
+  } catch (e) {
+    next(e);
+  }
 });
 
-router.delete("/:id", async (req, res) => {
-  let response = await YogiCollection.delete(req.params.id);
-  res.status(200).send("number of rows deleted" + response);
+router.delete("/:id", async (req, res, next) => {
+  try {
+    let response = await YogiCollection.delete(req.params.id);
+    res.status(200).send("number of rows deleted" + response);
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
